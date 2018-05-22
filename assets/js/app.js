cad13efe0a598912fa3132525b416e0e85acc421
Gareth Johns <info@garethjohnsdesign.com>
2018-05-22 23:41:56 +0100

    Add Page Transitions

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,25 @@ window.addEventListener('load', AOS.refresh);
 });
 
 
-$('[data-curtain-menu-button]').click(function(){
-  $('body').toggleClass('curtain-menu-open');
-})
+// 2. Turbolinks
+// -------------
+
+$(document).on('turbolinks:click', function(){
+  $('.page-content')
+    .addClass('animated fadeOut')
+    .off('webkitAnimationEnd oanimationend msAnimationEnd animationend')
+});
+
+
+$(document).on('turbolinks:load', function(event){
+    // if call was fired by turbolinks
+    if (event.originalEvent.data.timing.visitStart) { 
+      $('.page-content')
+        .addClass('animated fadeIn')
+        .one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+          $('.page-content').removeClass('animated');
+        });
+    }else{
+      $('.page-content').removeClass('hide')
+    } 
+ });
