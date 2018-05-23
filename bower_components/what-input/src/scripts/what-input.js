13c8f812d63e1e8702e26a6e884c9942f0f82f19
Gareth Johns <info@garethjohnsdesign.com>
2018-05-23 08:58:21 +0100

    Add Lazysize

--- bower_components/what-input/src/scripts/what-input.js
+++ bower_components/what-input/src/scripts/what-input.js
@@ -1,5 +1,30 @@
 module.exports = (() => {
   /*
+   * bail out if there is no document or window
+   * (i.e. in a node/non-DOM environment)
+   *
+   * Return a stubbed API instead
+   */
+  if (typeof document === 'undefined' || typeof window === 'undefined') {
+    return {
+      // always return "initial" because no interaction will ever be detected
+      ask: () => 'initial',
+
+      // always return null
+      element: () => null,
+
+      // no-op
+      ignoreKeys: () => {},
+
+      // no-op
+      registerOnChange: () => {},
+
+      // no-op
+      unRegisterOnChange: () => {}
+    }
+  }
+
+  /*
    * variables
    */
 
@@ -14,6 +39,18 @@ module.exports = (() => {
 
   // last used input intent
   let currentIntent = currentInput
+
+  // check for sessionStorage support
+  // then check for session variables and use if available
+  if (window.sessionStorage) {
+    if (window.sessionStorage.getItem('what-input')) {
+      currentInput = window.sessionStorage.getItem('what-input')
+    }
+
+    if (window.sessionStorage.getItem('what-intent')) {
+      currentIntent = window.sessionStorage.getItem('what-intent')
+    }
+  }
 
   // event buffer timer
   let eventTimer = null
@@ -153,6 +190,11 @@ module.exports = (() => {
 
       if (currentInput !== value && shouldUpdate) {
         currentInput = value
+
+        if (window.sessionStorage) {
+          window.sessionStorage.setItem('what-input', currentInput)
+        }
+
         doUpdate('input')
       }
 
@@ -166,6 +208,11 @@ module.exports = (() => {
 
         if (notFormInput) {
           currentIntent = value
+
+          if (window.sessionStorage) {
+            window.sessionStorage.setItem('what-intent', currentIntent)
+          }
+
           doUpdate('intent')
         }
       }
@@ -197,6 +244,11 @@ module.exports = (() => {
 
       if (currentIntent !== value) {
         currentIntent = value
+
+        if (window.sessionStorage) {
+          window.sessionStorage.setItem('what-intent', currentIntent)
+        }
+
         doUpdate('intent')
       }
     }
@@ -355,7 +407,7 @@ module.exports = (() => {
     unRegisterOnChange: fn => {
       let position = objPos(fn)
 
-      if (position) {
+      if (position || position === 0) {
         functionList.splice(position, 1)
       }
     }
