1887501caefdc17cb6258a4f53c51094fc9df965
Gareth Johns <info@garethjohnsdesign.com>
2019-11-06 14:39:21 +0000

    Updated Queens Park House and 25 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ import $ from "jquery";
 import Foundation from 'foundation-sites';
 import 'lightgallery';
 import AOS from 'aos';
+import Swiper from 'swiper';
 
 
 // 1. Special Queries
@@ -20,7 +21,6 @@ Foundation.Interchange.SPECIAL_QUERIES[ "xxlarge-retina" ] = 'only screen and (m
 
 $( document ).foundation();
 
-
 // 2. Animate on Scroll
 // --------------------
 
@@ -35,10 +35,51 @@ $(function() {
   window.addEventListener('load', AOS.refresh);
 });
 
-// 2. Animate on Scroll
-// --------------------
+// 3. Lightgallery
+// ---------------
 
 $(".gallery").lightGallery({
     selector: '.press__images',
     download: false
+});
+
+// 4. Carousel
+// -----------
+
+$(function() {
+  var firstSlide = Math.floor(Math.random() * 5);
+  var mySwiper = new Swiper ('.swiper-container', {
+    effect: 'fade',
+    slidesPerView: '1',
+    initialSlide: firstSlide,
+    loop: true,
+    preventClicks: true,
+    autoplay: {
+    delay: 3000,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-test',
+      prevEl: '.swiper-button-prev-test',
+    }
+  })
+});
+
+// 4. Viewport Height Fix
+// ----------------------
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+const vh = window.innerHeight * 0.01;
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
